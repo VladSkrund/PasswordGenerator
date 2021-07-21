8e8a06b6f086a81b1c6f20350e4e4f1ad4401f48
VladSkrund <vladskrund20@gmail.com>
2021-07-21 22:41:30 +0300

    Добавьте файлы проекта.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int PasswordLenght;
+            string pswrd = string.Empty;
+            Random rnd = new Random();
+            Console.WriteLine("Enter desired lenght of a password");
+            PasswordLenght = Convert.ToInt32(Console.ReadLine());
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ@$%&?#*{|abcdifghijklmnopqrstuvwxyz".ToCharArray();
+            
+
+            for (int i = 0; i < PasswordLenght; i++)
+            {
+                int num = rnd.Next(1,PasswordLenght);
+                if (num%2 == 0)
+                {
+                    int rndnum = rnd.Next(0, letters.Length);
+                    pswrd += letters[rndnum];
+
+                }
+                else
+                {
+                    pswrd += Convert.ToString(Randomizer());
+
+                }
+
+            }
+            Console.Write(pswrd);
+            Console.ReadKey();
+        }
+        static int Randomizer()
+        {
+
+            Random rnd = new Random();
+            int value = rnd.Next(0, 9);
+
+            return value;
+        }
+    }
+}
